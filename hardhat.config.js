b61b5d492a9c6089f6081d39430455cc23bf53e5
Sefa Tuncer <sefatuncer11@gmail.com>
2023-09-28 02:42:54 +0300

    Update delete hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+      swisstronik: {
+        url: "https://json-rpc.testnet.swisstronik.com/", 
+        accounts: ["0x${privateKey}"], 
+        // your account's private key 256 bit
+        // that's why i have deleted my private key because of it belongs to me
+      },
+    },
+};
